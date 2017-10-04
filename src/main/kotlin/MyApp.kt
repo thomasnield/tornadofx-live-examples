8ef30a99e051ee908aded257a1c67a60630993dd
Thomas Nield <thomasnield@live.com>
2017-10-03 17:26:22 -0700

    update example5

--- src/main/kotlin/MyApp.kt
+++ src/main/kotlin/MyApp.kt
@@ -24,7 +24,6 @@ class PatientsList : View("Patients") {
             column("AGE", Patient::ageBinding)
             column("WBCC", Patient::whiteBloodCellCountProperty).makeEditable()
 
-            isEditable = true
             enableDirtyTracking()
 
             subscribe<SavePatientEvent> {
